36b650612373a32c60730eaed193dc31a90056c5
drevenb <drevenb@gmail.com>
2021-05-25 15:08:22 +0300

    Add customer entity

---
+++ src/main/java/com/freeit/onlinestore/entity/Customer.java
@@ -0,0 +1,33 @@
+package com.freeit.onlinestore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Customer extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    private String address;
+}

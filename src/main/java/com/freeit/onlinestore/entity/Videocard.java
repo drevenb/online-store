32cbbcfcf9370c2695385f59d1b6eafe490aeb51
drevenb <drevenb@gmail.com>
2021-06-07 13:38:00 +0300

    Add videocard entity

--- src/main/java/com/freeit/onlinestore/entity/Videocard.java
+++ src/main/java/com/freeit/onlinestore/entity/Videocard.java
@@ -1,7 +1,42 @@
 package com.freeit.onlinestore.entity;
 
-import javax.persistence.Entity;
+import com.freeit.onlinestore.model.VideoMemoryType;
+import com.freeit.onlinestore.model.VideocardProducer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="videocard")
+@EqualsAndHashCode(callSuper = true)
 public class Videocard extends AbstractEntity {
+
+    @Column(name="graphic_processor")
+    private String graphicProcessor;
+
+    @Column(name="memory_size")
+    private Integer memorySize;
+
+    @Column(name="memory_buswidth")
+    private Integer memoryBuswidth;
+
+    @Column(name="memory_frequency")
+    private Integer memoryFrequency;
+
+    @Column(name="processor_frequency")
+    private Integer processorFrequency;
+
+    @Column(name="video_memory_type")
+    @Enumerated(EnumType.STRING)
+    private VideoMemoryType memoryType;
+
+    @Column(name="producer")
+    @Enumerated(EnumType.STRING)
+    private VideocardProducer producer;
 }
---
+++ src/main/java/com/freeit/onlinestore/model/VideocardProducer.java
@@ -0,0 +1,13 @@
+package com.freeit.onlinestore.model;
+
+public enum VideocardProducer {
+    PALIT,
+    ASUS,
+    MSI,
+    GIGABYTE,
+    AMD,
+    NVIDIA,
+    XFX,
+    ZOTAC,
+    POWERCOLOR
+}

4a9b85dabfca9574256fbce1db95ed48d522a1f4
drevenb <drevenb@gmail.com>
2021-06-07 16:14:47 +0300

    add ram entity

--- src/main/java/com/freeit/onlinestore/entity/Ram.java
+++ src/main/java/com/freeit/onlinestore/entity/Ram.java
@@ -1,7 +1,36 @@
 package com.freeit.onlinestore.entity;
 
-import javax.persistence.Entity;
+import com.freeit.onlinestore.model.MemoryType;
+import com.freeit.onlinestore.model.RamProducer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
 @Entity
+@Table(name="ram")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Ram extends AbstractEntity {
+
+    @Column(name="memory_size", nullable = false)
+    private Integer memorySize;
+
+    @Column(name="memory_frequency", nullable = false)
+    private Integer memoryFrequency;
+
+    @Column(nullable = false)
+    private String latency;
+
+    @Column(name="memory_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemoryType memoryType;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RamProducer producer;
 }

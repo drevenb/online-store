0de76fad46b9db636705f3a4c4fbf5a6aa5b039a
drevenb <drevenb@gmail.com>
2021-06-09 12:50:31 +0300

    insert test data1

---
+++ src/main/resources/db/migration/V5__insert_test_data1.sql
@@ -0,0 +1,110 @@
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130001', '0698ffaa-a79f-11eb-bcbc-0242ac130001', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130002', '0698ffaa-a79f-11eb-bcbc-0242ac130002', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130003', '0698ffaa-a79f-11eb-bcbc-0242ac130003', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130004', '0698ffaa-a79f-11eb-bcbc-0242ac130004', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130005', '0698ffaa-a79f-11eb-bcbc-0242ac130005', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130006', '0698ffaa-a79f-11eb-bcbc-0242ac130006', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130007', '0698ffaa-a79f-11eb-bcbc-0242ac130007', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130008', '0698ffaa-a79f-11eb-bcbc-0242ac130008', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130009', '0698ffaa-a79f-11eb-bcbc-0242ac130009', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130010', '0698ffaa-a79f-11eb-bcbc-0242ac130010', null, null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130011', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130001', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130012', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130002', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130013', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130003', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130014', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130004', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130015', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130005', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130016', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130006', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130017', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130007', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130018', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130008', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130019', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130009', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130020', null, '8ba50dc4-a79f-11eb-bcbc-0242ac130010', null, null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130021', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130001', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130022', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130002', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130023', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130003', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130024', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130004', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130025', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130005', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130026', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130006', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130027', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130007', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130028', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130008', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130029', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130009', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130030', null, null, 'f8fcba8e-a79f-11eb-bcbc-0242ac130010', null);
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130031', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130001');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130032', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130002');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130033', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130003');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130034', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130004');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130035', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130005');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130036', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130006');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130037', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130007');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130038', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130008');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130039', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130009');
+insert into product
+values ('a5f3468e-a7df-11eb-bc56-02abac130040', null, null, null, '201398a0-a79f-11eb-bcbc-0242ac130010');
+
+insert into ordering
+values ('a5f3468e-a7df-11eb-bc56-02abac130001', '2021-04-02 00:00:00.000000', 'PROCESSING', 899.49, '7889d51e-a79d-11eb-bcbc-0242ac130001');
+insert into ordering
+values ('a5f3468e-a7df-11eb-bc56-02abac130002', '2021-04-02 00:00:00.000000', 'PROCESSING', 2956.21, '7889d51e-a79d-11eb-bcbc-0242ac130002');
+insert into ordering
+values ('a5f3468e-a7df-11eb-bc56-02abac130003', '2021-04-02 00:00:00.000000', 'DELIVERED', 8120.23, '7889d51e-a79d-11eb-bcbc-0242ac130003');
+insert into ordering
+values ('a5f3468e-a7df-11eb-bc56-02abac130004', '2021-05-07 00:00:00.000000', 'PAID', 187.91, '7889d51e-a79d-11eb-bcbc-0242ac130004');
+insert into ordering
+values ('a5f3468e-a7df-11eb-bc56-02abac130005', '2021-04-02 00:00:00.000000', 'PAID_AND_DELIVERED', 741.07, '7889d51e-a79d-11eb-bcbc-0242ac130005');
+
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130001', 'a5f3468e-a7df-11eb-bc56-02abac130001');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130011', 'a5f3468e-a7df-11eb-bc56-02abac130001');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130022', 'a5f3468e-a7df-11eb-bc56-02abac130001');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130036', 'a5f3468e-a7df-11eb-bc56-02abac130002');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130038', 'a5f3468e-a7df-11eb-bc56-02abac130003');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130040', 'a5f3468e-a7df-11eb-bc56-02abac130003');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130027', 'a5f3468e-a7df-11eb-bc56-02abac130004');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130006', 'a5f3468e-a7df-11eb-bc56-02abac130005');
+insert into ordering_product
+values ('a5f3468e-a7df-11eb-bc56-02abac130016', 'a5f3468e-a7df-11eb-bc56-02abac130005');
